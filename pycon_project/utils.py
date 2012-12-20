0063ab367201f10b9a11e8c1bc1cc117ca8f9233
Jim Yeh <lemonlatte@gmail.com>
2012-12-20 23:17:41 +0800

    Move the middleware into a independent file

--- pycon_project/utils.py
+++ pycon_project/utils.py
@@ -3,19 +3,8 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
 from django.contrib.sites.models import Site
-from django.utils import translation
 
 from mailer import send_html_mail
-
-
-class I18NUrl(object):
-
-    def process_request(self, request):
-        lang = request.path.split("/")[1]
-        if lang in ["en", "zh_tw"]:
-            translation.activate(lang)
-            request.LANGUAGE_CODE = translation.get_language()
-            request.session['django_language'] = request.LANGUAGE_CODE
 
 
 def send_email(to, kind, **kwargs):
