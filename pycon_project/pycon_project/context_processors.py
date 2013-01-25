76c752b540bd5f22b2ae99183df1adbeed3b71e4
Jim Yeh <lemonlatte@gmail.com>
2013-01-26 01:58:54 +0800

    Make the i18n work with the nginx prefix

--- pycon_project/pycon_project/context_processors.py
+++ pycon_project/pycon_project/context_processors.py
@@ -7,7 +7,12 @@ def language_path(request):
     paths = request.path.split('/')
     for code, name in LANGUAGES:
         codes.append(code)
-    if paths[1] in codes:
-        del paths[1]
+    try:
+        if paths[1] in codes:
+            del paths[1]
+        if paths[2] in codes:
+            del paths[2]
+    except:
+        pass
     context_extras["no_lang_path"] = '/'.join(paths)
     return context_extras
